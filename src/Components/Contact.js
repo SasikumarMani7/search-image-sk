59e5d68d1198c31acee0ae883fb159886fe08d84
SasikumarMani7 <sasikumarkrmani@gmail.com>
2022-08-25 21:48:14 -0400

    second commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -6,6 +6,7 @@ const Contact = () => {
             style={{marginTop:'80px'}}>
             <h1>Contact</h1>
                 <p>
+                Sasikumar Mani    
                 Pexels provides high quality and completely free stock photos licensed under the Pexels license.
 
                 All photos are nicely tagged, searchable and also easy to discover through our discover pages.
